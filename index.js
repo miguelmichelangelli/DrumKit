ef673d9c92664c3fb3bc273dfbc41612adff6944
Miguel Michelangelli <109600065+miguelmichelangelli@users.noreply.github.com>
2024-03-27 14:56:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const drums = document.querySelectorAll(".drum");
+
+const numberOfDrums = drums.length;
+
+function drumBeat(){
+    var audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();
+}
+
+
+//Detect Button Press
+for(let i = 0; i < numberOfDrums; i++){
+
+    drums[i].addEventListener("click", function(){
+        var innerButton = this.innerHTML;
+
+        makeSound(innerButton);
+
+        buttonAnnimation(innerButton);
+    });
+
+}
+
+//Detect Keyboard Press
+document.addEventListener("keydown", function(event){
+    
+    makeSound(event.key)
+
+    buttonAnnimation(event.key);
+})
+
+
+function makeSound(key){
+
+    switch (key){
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+        break;
+
+        case "k":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+        break;
+
+        case "l":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+        break;
+
+        default:
+            console.log(innerButton);
+    }
+}
+
+function buttonAnnimation(currenKey){
+    var selectedButton= document.querySelector("." + currenKey);
+
+    selectedButton.classList.toggle("pressed");
+
+    setTimeout(function(){
+        selectedButton.classList.toggle("pressed");
+    }, 100);
+}
